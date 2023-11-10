9c9f8008001b4e89f3646654a09f8707fee681b9
jabongg <jbpvns@gmail.com>
2023-11-10 18:04:43 +0530

    added checbox and range input types

--- src/App.jsx
+++ src/App.jsx
@@ -26,31 +26,56 @@ function App() {
 
   return (
     <>
-    <h1 className="text-4xl text-center"> Password Generator</h1>
 
     <div className="w-full max-w-md mx-auto shadow-md raounded-lg x-4 my-8 text-orange-500 bg-gray-800">
-      test
+    <h1 className="text-4xl text-center my-3"> Password Generator</h1>
+
+    <div className="flex shadow rounded-lg overflow-hidden mb-4">
+      <input type="text" 
+      value={Password} 
+      className="outline-none w-full py-1 px-3"
+      placeholder="pasSWorD"
+      readOnly>
+
+      </input>
+      <button className="outline-none bg-blue-700 text-white px-3 py-1 shrink-0">copy</button>
+      </div>
+
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+      <input type="range" 
+      value={length} 
+      className="cursor-pointer"
+      min={8}
+      max={24} 
+      onChange={(e) => {setLength(e.target.value)}}
+      />
+      <label>Length : {length} </label>
+      </div>
+
+
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+      <input type="checkbox" 
+      value={length} 
+      defaultChecked={numberAllowed}
+     id="numberInput"
+      max={24} 
+      onChange={() => {setNumberAllowed((prev) => !prev)}}
+      />
+      <label htmlFor='numberInput'>Numbers</label>
+      </div>
+
+
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+      <input type="checkbox" 
+      value={length} 
+      defaultChecked={characterAllowed}
+     id="charInput"
+      onChange={() => {setCharacterAllowed((prev) => !prev)}}
+      />
+      <label htmlFor='charInput'>Characters</label>
+      </div>
     </div>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
     </>
   )
 }
